6aa609fab3b525b0abd9a703ba35dccec4627dcd
Jorge Rodríguez <123851986+jorgele054@users.noreply.github.com>
2023-03-20 14:24:24 -0500

    +1

--- scripts/Taller4_Descriptivas.R
+++ scripts/Taller4_Descriptivas.R
@@ -14,7 +14,9 @@
 
 library(pacman) 
 p_load(tidyverse, janitor, tm, stringi, tidytext, stopwords, wordcloud2, udpipe,
-       ggcorrplot) 
+       ggcorrplot, readtext, RColorBrewer,broom, rgdal) 
 
 train <- read.csv("C:/Users/jorge/Desktop/BIG DATA & ML/Problem Set 4/train.csv", comment.char="#")
 test <- read.csv("C:/Users/jorge/Desktop/BIG DATA & ML/Problem Set 4/test.csv", comment.char="#")
+
+PAPERS_CORPUS <- corpus(train$text)
