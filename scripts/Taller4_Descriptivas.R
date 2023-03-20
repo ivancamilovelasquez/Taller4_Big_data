2a0139183ae6617a47a5a83c2a78739a1aacfa52
Jorge Rodríguez <123851986+jorgele054@users.noreply.github.com>
2023-03-20 17:11:44 -0500

    diversidad

--- scripts/Taller4_Descriptivas.R
+++ scripts/Taller4_Descriptivas.R
@@ -19,8 +19,16 @@ p_load(tidyverse, janitor, tm, stringi, tidytext, stopwords, wordcloud2, udpipe,
 train <- read.csv("C:/Users/jorge/Desktop/BIG DATA & ML/Problem Set 4/train.csv", comment.char="#")
 test <- read.csv("C:/Users/jorge/Desktop/BIG DATA & ML/Problem Set 4/test.csv", comment.char="#")
 
+train_text <- data.frame(train) 
+PAPERS_CORPUS <- corpus(train_text$text)
+PALABRAS <- tokens(PAPERS_CORPUS, remove_punct = TRUE, remove_separators = TRUE, 
+                   remove_numbers = TRUE, verbose = TRUE )
+stopwords("es")
+DFM_PAPERS <- dfm(PALABRAS, remove = stopwords("es"))
+dfmat_inaug <- dfm(PALABRAS, remove = stopwords("es"))
+tstat_lexdiv <- textstat_lexdiv(dfmat_inaug)
+tail(tstat_lexdiv, 5)
 
-PAPERS_CORPUS <- corpus(train$text)
 
 
 
