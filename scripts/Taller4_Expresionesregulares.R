96846f99366242fb05b2c65ecc83bb429d422a25
ivancamilovelasquez <i.velasqueza@uniandes.edu.co>
2023-03-20 14:53:43 -0500

    expresiones

--- scripts/Taller4_Expresionesregulares.R
+++ scripts/Taller4_Expresionesregulares.R
@@ -20,14 +20,59 @@ library(pacman)
 p_load(tidyverse, ggplot2, openxlsx, scales, skimr, stringi, SnowballC, stringr)
 
 
+# Dejar bases de comentarios por politicos 
+
+petro <-  subset(train, name == "Petro", select = c(comentario))
+uribe <-  subset(train, name == "Uribe", select = c(comentario))
+lopez <-  subset(train, name == "Lopez", select = c(comentario))
+
+# Dejar a nivel palabras 
+petro <- petro %>%
+  mutate(id = row_number())
+uribe <- uribe %>%
+  mutate(id = row_number())
+lopez <- lopez %>%
+  mutate(id = row_number())
+
+words_petro <- petro %>%
+  unnest_tokens(output = "word", input = "comentario")
+
+words_uribe <- uribe %>%
+  unnest_tokens(output = "word", input = "comentario")
+
+words_lopez <- lopez %>%
+  unnest_tokens(output = "word", input = "comentario")
+
+
+
+# Mapa de palabras
+
+words_lopez <-  subset(words_lopez, word != "ano")
+n_words_lopez <- words_lopez %>%
+  count(word) %>%
+  arrange(desc(n)) %>%
+  head(100)
+wordcloud2(data = n_words_lopez)
+
+
+n_words_petro <- words_petro %>%
+  count(word) %>%
+  arrange(desc(n)) %>%
+  head(100)
+wordcloud2(data = n_words_petro)
+
+
+n_words_uribe <- words_uribe %>%
+  count(word) %>%
+  arrange(desc(n)) %>%
+  head(100)
+wordcloud2(data = n_words_uribe)
+
 
 objetos <- c("train", "test")
-#objetos <- c("train", "test")
 for (obj in objetos) {
-  
-  
-data <- get(obj) 
 
+data <- get(obj) 
 
 data$Bogota <- grepl("\\bogota\\b", data$comentario)
 data$colombiahumana <- grepl("\\colombiahumana\\b", data$comentario)
@@ -40,6 +85,8 @@ data$transmilenio <- grepl("\\transmilenio\\b", data$comentario)
 data$covid <- grepl("\\covid\\b", data$comentario)
 data$distrital <- grepl("\\covid\\b", data$comentario)
 data$estacion <- grepl("\\estacion\\b", data$comentario)
+
+
 
 assign(obj, data)
 rm(data)
