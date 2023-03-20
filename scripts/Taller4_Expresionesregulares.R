9d2f10085668679aecaf88aed388170ae67b182b
ivancamilovelasquez <i.velasqueza@uniandes.edu.co>
2023-03-20 13:33:29 -0500

    expresioens regulares
    expresioens regulares

--- scripts/Taller4_Expresionesregulares.R
+++ scripts/Taller4_Expresionesregulares.R
@@ -21,5 +21,28 @@ p_load(tidyverse, ggplot2, openxlsx, scales, skimr, stringi, SnowballC, stringr)
 
 
 
+objetos <- c("train", "test")
+#objetos <- c("train", "test")
+for (obj in objetos) {
+  
+  
+data <- get(obj) 
 
-data$apartamento_m <- grepl("\\bapartamento\\b", data$title_modificado)
+
+data$Bogota <- grepl("\\bogota\\b", data$comentario)
+data$colombiahumana <- grepl("\\colombiahumana\\b", data$comentario)
+data$cundinamarcar <- grepl("\\cundinamarcar\\b", data$comentario)
+data$concejo <- grepl("\\concejo\\b", data$comentario)
+data$gobierno <- grepl("\\gobierno\\b", data$comentario) 
+data$socialismo <- grepl("\\socialismo\\b", data$comentario) 
+data$fascismo <- grepl("\\fascismo\\b", data$comentario)
+data$transmilenio <- grepl("\\transmilenio\\b", data$comentario)
+data$covid <- grepl("\\covid\\b", data$comentario)
+data$distrital <- grepl("\\covid\\b", data$comentario)
+distrital 
+
+assign(obj, data)
+rm(data)
+
+}
+
